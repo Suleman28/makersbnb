aec0a11d3532715cf4a9cfadcc75f57a5dcbeaf8
Charlie Sampson <153772582+sxmz03@users.noreply.github.com>
2026-09-01 12:41:52 +0100

    booking repository

---
+++ lib/booking_repository.py
@@ -0,0 +1,35 @@
+from lib.booking import Booking 
+
+class BookingRepository:
+
+    # We initialise with a database connection
+    def __init__(self, connection):
+        self._connection = connection
+
+    # Retrieve all bookings
+    def all(self):
+        rows = self._connection.execute('SELECT * from bookings')
+        bookings = []
+        for row in rows:
+            item = Booking(row["id"], row["start_date"], row["end_date"], row["status"], row["listing_id"])
+            bookings.append(item)
+        return bookings
+
+    # Find a single booking by its id
+    def find(self, booking_id):
+        rows = self._connection.execute(
+            'SELECT * from bookings WHERE id = %s', [booking_id])
+        row = rows[0]
+        return Booking(row["id"], row["start_date"], row["end_date"], row["status"], row["listing_id"])
+
+    # Create a new booking
+    def create(self, booking):
+        self._connection.execute('INSERT INTO bookings (start_date, end_date, status, listing_id) VALUES (%s, %s, %s, %s)', [
+                                 booking.start_date, booking.end_date, booking.status, booking.listing_id])
+        return None
+
+    # Delete a booking by its id
+    def delete(self, booking_id):
+        self._connection.execute(
+            'DELETE FROM bookings WHERE id = %s', [booking_id])
+        return None
