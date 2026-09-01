40a11be4fbadba357d421ab94e5e56eaa6a399bc
ryan.osmaston <ryan.osmaston@gmail.com>
2026-09-01 16:31:54 +0100

    redesign templates to include new features

--- app.py
+++ app.py
@@ -2,48 +2,54 @@ import os
 from flask import Flask, request, render_template, redirect, flash
 from werkzeug.security import generate_password_hash
 from lib.database_connection import get_flask_database_connection
+from lib.listing_repository import ListingRepository
 from lib.user import User
 from lib.user_repository import UserRepository
-from lib.listing_repository import ListingRepository
 
 
 app = Flask(__name__)
-app.secret_key = os.environ['SECRET_KEY']
+app.secret_key = os.environ.get("SECRET_KEY", "dev-secret-key")
 
-@app.route('/', methods=['GET'])
+
+@app.route("/", methods=["GET"])
+@app.route("/index", methods=["GET"])
 def get_index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/signup', methods=['GET'])
+
+@app.route("/signup", methods=["GET"])
 def get_signup():
-  return render_template('signup.html')
+    return render_template("signup.html")
 
-@app.route('/signup', methods=['POST'])
+
+@app.route("/signup", methods=["POST"])
 def create_account():
-  name = request.form['name']
-  email = request.form['email']
-  password = request.form['password']
-  password_confirmation = request.form['password_confirmation']
+    name = request.form["name"]
+    email = request.form["email"]
+    password = request.form["password"]
+    password_confirmation = request.form["password_confirmation"]
 
-  if password != password_confirmation:
-    return render_template('signup.html', error="Passwords do not match"), 400
+    if password != password_confirmation:
+        return render_template("signup.html", error="Passwords do not match"), 400
 
-  connection = get_flask_database_connection(app)
-  repository = UserRepository(connection)
+    connection = get_flask_database_connection(app)
+    repository = UserRepository(connection)
 
-  hashed_password = generate_password_hash(password)
-  user = User(name, email, hashed_password)
-  repository.create(user)
+    hashed_password = generate_password_hash(password)
+    user = User(name, email, hashed_password)
+    repository.create(user)
 
-  flash("Signed up successfully!")
-  return redirect('/')
+    flash("Signed up successfully!")
+    return redirect("/")
 
-@app.route('/listings', methods=['GET'])
-def get_home():
+
+@app.route("/listings", methods=["GET"])
+def get_listings():
     connection = get_flask_database_connection(app)
     repository = ListingRepository(connection)
     listings = repository.all()
-    return render_template('listings.html', listings=listings)
+    return render_template("listings.html", listings=listings)
 
-if __name__ == '__main__':
-    app.run(debug=True, port=int(os.environ.get('PORT', 5001)))
+
+if __name__ == "__main__":
+    app.run(debug=True, port=int(os.environ.get("PORT", 5001)))
