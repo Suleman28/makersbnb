e1a4e7e57c4bae03dbef2a1d7487afca1384c5e1
rochelle <133251357+ChelleRB@users.noreply.github.com>
2026-09-02 13:20:06 +0100

    small bug

--- tests/test_listing_model.py
+++ tests/test_listing_model.py
@@ -1,6 +1,8 @@
 import sys
 import os 
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from lib.listing import Listing
 
 def testing_clifftop_retreat_seed_listing_works():
