01719d432a72c8445ea418273702e8c371408bc5
Emily Blackford <emilyroseblackford@gmail.com>
2026-09-02 14:16:24 +0100

    Updated user and booking repo tests to reflect seed data changes

--- tests/test_booking_repository.py
+++ tests/test_booking_repository.py
@@ -1,7 +1,8 @@
+from datetime import date
+
 from lib.booking import Booking
 from lib.booking_repository import BookingRepository
 from lib.database_connection import DatabaseConnection
-from datetime import date
 
 def test_get_all_bookings():
     connection = DatabaseConnection(test_mode=True)
@@ -12,8 +13,9 @@ def test_get_all_bookings():
     bookings = repo.all()
 
     assert bookings == [
-        Booking(date(2027, 1, 5), "2027-01-10", "PENDING", 2, 1),
-        Booking("2027-01-05", "2027-01-10", "PENDING", 3, 2),
-        Booking("2027-01-11", "2027-01-13", "BOOKED", 3, 3)
+        Booking(date(2027, 1, 5), date(2027, 1, 10), "PENDING", 1, 1),
+        Booking(date(2027, 1, 5), date(2027, 1, 12), "PENDING", 2, 2),
+        Booking(date(2027, 1, 11), date(2027, 1, 13), "BOOKED", 1, 3),
+        Booking(date(2027, 1, 23), date(2027, 1, 25), "BOOKED", 3, 4)
     ]
 
--- tests/test_user_repository.py
+++ tests/test_user_repository.py
@@ -11,9 +11,10 @@ def test_all_returns_all_users():
     users = repo.all()
 
     assert users == [
-        User("Peter Puffin", "pp@email.com", "12345678", "12345678", 1),
-        User("Polly Penguin", "pollyp@email.com", "12345678", "12345678", 2),
-        User("Quentin Quail", "qq@email.com", "12345678", "12345678", 3)
+        User("Peter Puffin", "pp@email.com", "12445778", 1),
+        User("Polly Penguin", "pollyp@email.com", "12345678", 2),
+        User("Quentin Quail", "qq@email.com", "12649670", 3),
+        User("Spears Sparrow", "spears@email.com", "12242650", 4)
     ]
 
 def test_find_returns_the_correct_user():
@@ -24,7 +25,7 @@ def test_find_returns_the_correct_user():
 
     user_to_find = repo.find(1)
 
-    assert user_to_find == User("Peter Puffin", "pp@email.com", "12345678", "12345678", 1)
+    assert user_to_find == User("Peter Puffin", "pp@email.com", "12445778", 1)
 
 def test_create_new_user_adds_to_users():
     connection = DatabaseConnection(test_mode=True)
@@ -34,15 +35,14 @@ def test_create_new_user_adds_to_users():
 
     new_user = User("Clive Crane",
                     "cc@email.com",
-                    "password1",
-                    "password1",
+                    "29572154",
                     4)
 
     repo.create(new_user)
 
     users = repo.all()
 
-    assert len(users) == 4
+    assert len(users) == 5
 
 def test_delete_user_removes_entry_from_list():
     connection = DatabaseConnection(test_mode=True)
@@ -55,8 +55,9 @@ def test_delete_user_removes_entry_from_list():
     users = repo.all()
 
     assert users == [
-        User("Polly Penguin", "pollyp@email.com", "12345678", "12345678", 2),
-        User("Quentin Quail", "qq@email.com", "12345678", "12345678", 3)
+        User("Polly Penguin", "pollyp@email.com", "12345678", 2),
+        User("Quentin Quail", "qq@email.com", "12649670", 3),
+        User("Spears Sparrow", "spears@email.com", "12242650", 4)
     ]
 
 
