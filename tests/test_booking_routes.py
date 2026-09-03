810536d0946e322db3ed4c623e339e4c79323c2e
ryan.osmaston <ryan.osmaston@gmail.com>
2026-09-03 14:43:16 +0100

    bugfix for booking routes

--- tests/test_booking_routes.py
+++ tests/test_booking_routes.py
@@ -23,7 +23,6 @@ def test_create_booking_requires_login(web_client, db_connection):
     )
     assert response.status_code == 302
     assert response.location == "/sign_in"
-
     rows = db_connection.execute(
         "SELECT * FROM bookings WHERE start_date = %s AND end_date = %s",
         ["2027-02-01", "2027-02-05"],
@@ -32,8 +31,7 @@ def test_create_booking_requires_login(web_client, db_connection):
 
 
 def test_create_booking_success(web_client, db_connection):
-    sign_in(web_client, "pp@email.com", "12445778")  # user id 1
-
+    sign_in(web_client, "pp@email.com", "12445778")
     response = web_client.post(
         "/bookings",
         data={
@@ -44,7 +42,6 @@ def test_create_booking_success(web_client, db_connection):
     )
     assert response.status_code == 302
     assert response.location == "/listings/1"
-
     rows = db_connection.execute(
         "SELECT * FROM bookings WHERE start_date = %s AND end_date = %s AND listing_id = %s",
         ["2027-02-01", "2027-02-05", 1],
@@ -54,9 +51,26 @@ def test_create_booking_success(web_client, db_connection):
 
 
 def test_create_booking_rejects_overlapping_dates(web_client, db_connection):
-    sign_in(web_client, "pp@email.com", "12445778")  # user id 1
+    sign_in(web_client, "pp@email.com", "12445778")
+    response = web_client.post(
+        "/bookings",
+        data={
+            "listing_id": "1",
+            "start_date": "2027-01-12",
+            "end_date": "2027-01-14",
+        },
+    )
+    assert response.status_code == 302
+    assert response.location == "/listings/1"
+    rows = db_connection.execute(
+        "SELECT * FROM bookings WHERE start_date = %s AND end_date = %s AND listing_id = %s",
+        ["2027-01-12", "2027-01-14", 1],
+    )
+    assert len(rows) == 0
 
-    # Listing 1 already has a PENDING booking for 2027-01-05 to 2027-01-10
+
+def test_create_booking_allows_overlapping_pending_dates(web_client, db_connection):
+    sign_in(web_client, "pp@email.com", "12445778")
     response = web_client.post(
         "/bookings",
         data={
@@ -67,12 +81,11 @@ def test_create_booking_rejects_overlapping_dates(web_client, db_connection):
     )
     assert response.status_code == 302
     assert response.location == "/listings/1"
-
     rows = db_connection.execute(
         "SELECT * FROM bookings WHERE start_date = %s AND end_date = %s AND listing_id = %s",
         ["2027-01-06", "2027-01-08", 1],
     )
-    assert len(rows) == 0
+    assert len(rows) == 1
 
 
 def test_get_travel_bookings_requires_login(web_client):
@@ -82,6 +95,6 @@ def test_get_travel_bookings_requires_login(web_client):
 
 
 def test_get_travel_bookings_when_signed_in(web_client):
-    sign_in(web_client, "pp@email.com", "12445778")  # user id 1
+    sign_in(web_client, "pp@email.com", "12445778")
     response = web_client.get("/users/1/bookings")
     assert response.status_code == 200
